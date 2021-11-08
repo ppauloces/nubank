3109d6de30061d66eb9a43707633c96af2b80740
Paulo Amaral <81058578+ppauloces@users.noreply.github.com>
2021-11-08 13:57:56 -0300

    Update card_app.dart

--- nubank/lib/pages/home/widgets/card_app.dart
+++ nubank/lib/pages/home/widgets/card_app.dart
@@ -6,7 +6,7 @@ class CardApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(20),
+      padding: const EdgeInsets.all(19),
       child: Container(
           decoration: BoxDecoration(
               color: Colors.white, borderRadius: BorderRadius.circular(5))),
